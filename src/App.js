6c79480d918b529048313996b327c3b2e7c22f5e
levin.kitty <levin.kitty@kakaocorp.com>
2021-06-29 11:10:47 +0900

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Homepage />
     </div>
   );
 }

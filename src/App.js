1405814743e99f5a0157fb2eb7ff1e2580dff90c
levin.kitty <levin.kitty@kakaocorp.com>
2021-07-02 23:14:46 +0900

    updating our app with shop page and collection preview component, collection item component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import Homepage from './pages/homepage/Homepage';
+import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={Homepage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
